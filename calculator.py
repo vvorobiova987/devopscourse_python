69da14412e994aec52aa794181c51ee701ec866e
Valeriia Vorobiova <vvorobiova987@gmail.com>
2023-07-04 19:59:16 +0200

    [HW1 python]A cycle has been added to verify if the input is correct.

--- calculator.py
+++ calculator.py
@@ -14,15 +14,20 @@ def multiplication(number1, number2):
 
 # Division function
 def division(number1, number2):
-    if float(number2) !=0:
-    	return float(number1) / float(number2)
-    else: 
-    	print("Error: Division by zero is not allowed!")
+    return float(number1) / float(number2)
 
+while True:
+    number1 = input("Number 1:")
+    number2 = input("Number 2:")
 
-number1 = input("Number 1:")
-number2 = input("Number 2:") 
-
+    if number1.isdigit() and number2.isdigit():
+        number1 = float(number1)
+        number2 = float(number2)
+        break
+    else:
+        print("Oops.Invalid input. Please enter numbers.")
+ 
+	
 operation = input("Please select the desired operation:\n\n 1. Addition (+) \n 2. Subtraction (-)\n 3. Multiplication (*) \n 4. Division (/)\n") 
 
 if operation == "1":
