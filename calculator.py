d1a2352be6ecee302b93f91bc6965428cbeca876
Valeriia Vorobiova <vvorobiova987@gmail.com>
2023-07-05 11:11:43 +0200

    [HW1 python] Added a prompt if the user wants to repeat the operation

--- calculator.py
+++ calculator.py
@@ -15,67 +15,78 @@ def multiplication(number1, number2):
 # Division function
 def division(number1, number2):
     return number1 / number2
-
 while True:
-    # First number for our calculator
-    number1 = input("Number 1: ")
 
-    # Check if the first input number is correct
-    if number1.isdigit():
-        number1 = int(number1)
-        break
-    else:
-        try:
-            number1 = float(number1)
+    while True:
+        # First number for our calculator
+        number1 = input("Number 1: ")
+
+        # Check if the first input number is correct
+        if number1.isdigit():
+            number1 = int(number1)
             break
-        except ValueError:
-            print("Error: Number 1 is neither an integer nor a float. Please try to write Number 1 again ")
+        else:
+            try:
+                number1 = float(number1)
+                break
+            except ValueError:
+                print("Error: Number 1 is neither an integer nor a float. Please try to write Number 1 again ")
 
-while True:
-    # Second number for our calculator
-    number2 = input("Number 2: ")
+    while True:
+        # Second number for our calculator
+        number2 = input("Number 2: ")
 
-    # Check if the second input number is correct
-    if number2.isdigit():
-        number2 = int(number2)
-        break
-    else:
-        try:
-            number2 = float(number2)
+        # Check if the second input number is correct
+        if number2.isdigit():
+            number2 = int(number2)
             break
-        except ValueError:
-            print("Error: Number 2 is neither an integer nor a float. Please try to write Number 2 again ")
+        else:
+            try:
+                number2 = float(number2)
+                break
+            except ValueError:
+                print("Error: Number 2 is neither an integer nor a float. Please try to write Number 2 again ")
 
-while True:
+    while True:
 
-#Choose the operation  
-    operation = input("\nPlease select operation:\n\n 1. Addition (+) \n 2. Subtraction (-)\n 3. Multiplication (*) \n 4. Division (/)\n") 
+    #Choose the operation  
+        operation = input("\nPlease select operation:\n\n 1. Addition (+) \n 2. Subtraction (-)\n 3. Multiplication (*) \n 4. Division (/)\n") 
 
-#Check if the operation is correct
-    if operation.isdigit() and int(operation) in range(1, 5):
-        operation = int(operation)
-        break
-    else:
-        print("Oops.Invalid input. Please try again")
+    #Check if the operation is correct
+        if operation.isdigit() and int(operation) in range(1, 5):
+            operation = int(operation)
+            break
+        else:
+            print("Oops.Invalid input. Please try again")
 
-if operation == 1:
-     result=addition(number1,number2)
-     print("Result:", result)
-elif operation == 2:
-     result=subtraction(number1,number2)
-     print("Result:", result)
-elif operation == 3:
-     result=multiplication(number1,number2)
-     print("Result:", result)
-elif operation == 4:
-     if number2 != 0:
-         result=division(number1,number2)
+    if operation == 1:
+         result=addition(number1,number2)
          print("Result:", result)
-     else:
-         print("Error: Division by zero is not allowed!")
+    elif operation == 2:
+         result=subtraction(number1,number2)
+         print("Result:", result)
+    elif operation == 3:
+         result=multiplication(number1,number2)
+         print("Result:", result)
+    elif operation == 4:
+         if number2 != 0:
+             result=division(number1,number2)
+             print("Result:", result)
+         else:
+             print("Error: Division by zero is not allowed!")
      
-else:
-    print ("Error")
+    else:
+        print ("Error")
 
+    
+
+    choice = input("Do you want to try again? (Y/N): ")
+    if choice.lower() == 'y':
+        print("Continue")
+    elif choice.lower() == 'n':
+        print("Exiting...")
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
 
 
